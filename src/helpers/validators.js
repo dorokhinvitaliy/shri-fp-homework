8375f8417050dfcf6c3b0ce275af9c07344da409
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-07-14 16:37:13 +0300

    part 1 done

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,5 @@
+import * as R from 'ramda';
+
 /**
  * @file Домашка по FP ч. 1
  *
@@ -14,37 +16,109 @@
  */
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+export const validateFieldN1 = (shapes) => {
+    const isStarRed = R.propEq("star", "red");
+    const isSquareGreen = R.propEq("square", "green");
+    const isTriangleWhite = R.propEq("triangle", "white");
+    const isCircleWhite = R.propEq("circle", "white");
 
-    return star === 'red' && square === 'green';
+    const isCorrect = R.allPass([
+        isStarRed,
+        isSquareGreen,
+        isTriangleWhite,
+        isCircleWhite,
+    ]);
+
+    return isCorrect(shapes);
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (shapes) => {
+    const isGreen = R.equals('green');
+    const colors = R.props(['star', 'square', 'triangle', 'circle'], shapes);
+    const greenOnly = R.filter(isGreen, colors);
+    return R.gte(R.length(greenOnly), 2);
+};
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (shapes) => {
+    const isBlue = R.equals('blue');
+    const isRed = R.equals('red');
+    const colors = R.props(['star', 'square', 'triangle', 'circle'], shapes);
+    const blueOnly = R.filter(isBlue, colors);
+    const redOnly = R.filter(isRed, colors);
+    return R.equals(R.length(blueOnly), R.length(redOnly));
+};
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = (shapes) => {
+    const isStarCorrect = R.propEq("star", "red");
+    const isSquareCorrect = R.propEq("square", "orange");
+    const isCircleCorrect = R.propEq("circle", "blue");
+
+    const isCorrect = R.allPass([
+        isStarCorrect,
+        isSquareCorrect,
+        isCircleCorrect,
+    ]);
+
+    return isCorrect(shapes);
+};
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = R.pipe(
+  R.props(['star', 'square', 'triangle', 'circle']),
+  R.reject(R.equals('white')),
+  R.countBy(R.identity),
+  R.values,
+  R.any(R.gte(R.__, 3))
+);
+
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (shapes) => {
+    const colors = R.props(['star', 'square', 'triangle', 'circle'], shapes);
+
+    const greenCount = R.pipe(
+        R.filter(R.equals('green')),
+        R.length
+    )(colors);
+
+    const redCount = R.pipe(
+        R.filter(R.equals('red')),
+        R.length
+    )(colors);
+
+    const isTriangleGreen = R.propEq('triangle', 'green');
+
+    return greenCount === 2 && redCount === 1 && isTriangleGreen(shapes);
+};
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = R.pipe(
+    R.props(['star', 'square', 'triangle', 'circle']),
+    R.all(R.equals('orange'))
+);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = R.pipe(
+    R.prop('star'),
+    R.allPass([
+        R.complement(R.equals('red')),
+        R.complement(R.equals('white'))
+    ])
+);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = R.pipe(
+    R.props(['star', 'square', 'triangle', 'circle']),
+    R.all(R.equals('green'))
+);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = (shapes) => {
+    const triangleColor = shapes.triangle;
+    const squareColor = shapes.square;
+
+    return triangleColor === squareColor && triangleColor !== 'white';
+};
